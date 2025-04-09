bf13e6d02aa0b1a63a35174a7f7a3e1950a43dab
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2025-04-09 19:14:57 +0330

    [ADD] Dashboard Page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use cleint";
+
+import { ReactElement } from "react";
+
+import styles from "./page.module.css";
+
+export default function Page(): ReactElement {
+  return <div className={styles.page}>داشبورد</div>;
+}
