c44f2a9e078fed49e6140793e178510d5910e1f6
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2025-04-11 13:10:03 +0330

    [ADD] ParsBody Function for Errors & Dto Type

--- src/app/api/auth/sign-up/route.tsx
+++ src/app/api/auth/sign-up/route.tsx
@@ -1,12 +1,20 @@
 import prisma from "@/lib/prisma";
+
+import { signupDto } from "@/dto/auth.dto";
+
+import { parsBody, wrapWithTryCash } from "@/utils/api.utils";
+
 import { ApiResponseType } from "@/types/api.response.tyle";
-import { wrapWithTryCash } from "@/utils/api.utils";
+
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request): Promise<ApiResponseType<null>> {
   return wrapWithTryCash(async () => {
-    const body = await request.json();
+    const [parseError, body] = await parsBody<signupDto>(request);
 
+    if (parseError !== null) {
+      return NextResponse.json({ data: parseError }, { status: 400 });
+    }
     await prisma.user.create({ data: body });
 
     return NextResponse.json({ data: null }, { status: 201 });
--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,6 +1,26 @@
 import { ApiResponseType } from "@/types/api.response.tyle";
 import { NextResponse } from "next/server";
 
+
+
+type ParsBodyResult<T> =[error:null , data:T]|[error:string , data:null];
+export async function parsBody<T>(request: Request):Promise<ParsBodyResult<T>> {
+    try {
+        const body = await request.json();
+        return [null,body]
+    } catch (error) {
+        if (error instanceof Error) {
+            return [error.message,null]
+          }
+          if (error ==="string") {
+            return [error ,null]
+          }
+          return ["خطای غیر منتظره رخ داده است",null]
+    }
+}
+
+
+
 export async function wrapWithTryCash<T>(callback :()=>Promise<ApiResponseType<T>>):Promise<ApiResponseType<T>>{
 try {
    return await callback()
@@ -14,3 +34,4 @@ try {
       );
 }
 }
+
